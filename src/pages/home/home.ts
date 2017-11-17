d934877667871040e7cc107acb277062f6e6265b
gs-ashwath <abharadwaj@gainsight.com>
2017-11-18 04:02:49 +0530

    Login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,41 +14,41 @@ import { Observable } from "rxjs/Observable";
 })
 export class HomePage {
 
-  // constructor(public navCtrl: NavController, public authProvider: AuthProvider) {}
+  constructor(public navCtrl: NavController, public authProvider: AuthProvider) {}
 
-  form: any = {
-    title: "",
-    content: ""
-  };
-  meals: Observable<any>;
-  constructor(public navCtrl: NavController, private store: Store<AppState>) {
-    this.meals = store.select<any>("meals");
-  }
-  addMeal() {
-    let id = Math.random().toString(36).substr(2, 10);
-    this.store.dispatch(
-      new MealActions.AddMeal({
-        id: id,
-        title: this.form.title,
-        content: this.form.content
-      })
-    );
-  }
-  removeMeal(_meal) {
-    this.store.dispatch(new MealActions.DeleteMeal({ id: _meal.id }));
-  }
-  resetMeals() {
-    this.store.dispatch(new MealActions.ResetMeal({}));
-  }
+  // form: any = {
+  //   title: "",
+  //   content: ""
+  // };
+  // meals: Observable<any>;
+  // constructor(public navCtrl: NavController, private store: Store<AppState>) {
+  //   this.meals = store.select<any>("meals");
+  // }
+  // addMeal() {
+  //   let id = Math.random().toString(36).substr(2, 10);
+  //   this.store.dispatch(
+  //     new MealActions.AddMeal({
+  //       id: id,
+  //       title: this.form.title,
+  //       content: this.form.content
+  //     })
+  //   );
+  // }
+  // removeMeal(_meal) {
+  //   this.store.dispatch(new MealActions.DeleteMeal({ id: _meal.id }));
+  // }
+  // resetMeals() {
+  //   this.store.dispatch(new MealActions.ResetMeal({}));
+  // }
 
   /**
    * Calls the authentication provider and logs the user out, on successful logout it sends the user
    * back to the login page.
    */
-  // logMeOut() {
-  //   this.authProvider.logoutUser().then( () => {
-  //     this.navCtrl.setRoot('LoginPage');
-  //   });
-  // }
+  logMeOut() {
+    this.authProvider.logoutUser().then( () => {
+      this.navCtrl.setRoot('LoginPage');
+    });
+  }
 
 }
